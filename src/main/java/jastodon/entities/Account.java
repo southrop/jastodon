732ec119c1a956311f2fe4712ab56af7c5151061
Southrop <southrop113@gmail.com>
2017-04-16 02:37:17 +0900

    Account was badly defined

--- src/main/java/jastodon/entities/Account.java
+++ src/main/java/jastodon/entities/Account.java
@@ -1,20 +1,41 @@
 package jastodon.entities;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Date;
+
 /**
  * @author Southrop
  */
 
 public class Account {
+
     public String id;
+
     public String username;
+
     public String acct;
+
     public String note;
+
     public String url;
+
     public String avatar;
+
     public String header;
+
     public boolean locked;
-    public int createdAt;
+
+    @SerializedName("created_at")
+    public Date createdAt;
+
+    @SerializedName("followers_count")
     public String followersCount;
+
+    @SerializedName("following_count")
     public String followingCount;
+
+    @SerializedName("statuses_count")
     public String statusesCount;
+
 }
