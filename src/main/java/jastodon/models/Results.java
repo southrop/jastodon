f4e86d1b382a050fd0970192cbe1ffccf0554280
Southrop <southrop113@gmail.com>
2017-04-28 19:15:10 +0900

    Missing definitions for search service

---
+++ src/main/java/jastodon/models/Results.java
@@ -0,0 +1,15 @@
+package jastodon.models;
+
+/**
+ * @author admin
+ */
+
+public class Results {
+
+    public Account[] accounts;
+
+    public Status[] statuses;
+
+    public Tag[] hashtags;
+
+}
---
+++ src/main/java/jastodon/models/Tag.java
@@ -0,0 +1,13 @@
+package jastodon.models;
+
+/**
+ * @author admin
+ */
+
+public class Tag {
+
+    public String name;
+
+    public String url;
+
+}
